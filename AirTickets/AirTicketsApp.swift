7fe0f1c0a63676602730da80a9bc444448962f88
Kristina Grebneva <kristina.o.grebneva@gmail.com>
2024-05-28 18:42:21 +0200

    Initial Commit

---
+++ AirTickets/AirTicketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirTicketsApp.swift
+//  AirTickets
+//
+//  Created by Kristina Grebneva on 28.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirTicketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
