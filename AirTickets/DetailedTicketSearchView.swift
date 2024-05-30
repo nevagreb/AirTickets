f719fccaa48217202d0f1f680607c33f7f66d578
Kristina Grebneva <kristina.o.grebneva@gmail.com>
2024-05-31 00:05:38 +0200

    Added detailed ticket search view, added Search bar view, fixed drawbacks of view model

---
+++ AirTickets/DetailedTicketSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailedTicketSearchView.swift
+//  AirTickets
+//
+//  Created by Kristina Grebneva on 30.05.2024.
+//
+
+import SwiftUI
+
+struct DetailedTicketSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailedTicketSearchView()
+}
---
+++ AirTickets/TicketsOffers.swift
@@ -0,0 +1,8 @@
+//
+//  TicketsOffers.swift
+//  AirTickets
+//
+//  Created by Kristina Grebneva on 30.05.2024.
+//
+
+import Foundation
